c3a311c87149eea335d5cfddd3ab9f038d1c2931
Parshva <codewithpasshu@gmail.com>
2025-02-17 10:42:15 +0530

    Unit-2

---
+++ U_2P_1.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title>Post Get</title>
+</head>
+<body>
+<form action="U_2P_1.php" method="post">
+<table>
+<tr>
+<td><lable for="name">Name</lable></td>
+<td><input type="text" name="fullname"></td>
+</tr>
+<tr>
+<td><lable for="Email">Email</lable></td>
+<td><input type="Email" name="Email"></td>
+</tr>
+<tr>
+<td colspan="2"><input type="submit" name="submit" value="submit">
+<input type="reset" name="reset" value="reset"></td>
+</tr>
+</table>
+</form>
+</body>
+<?php
+$fullname=$_POST["fullname"];
+$email=$_POST["Email"];
+echo "Form Submit following Data <br>";
+echo "Fullname=".$fullname."<br>";
+echo "Email=$email<br>";
+?>
+</html>
